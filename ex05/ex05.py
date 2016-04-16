6cd365cb23d13b766e32ebc75334f3a600fd06d7
park chan hui <877chan@naver.com>
2016-04-16 21:59:25 +0900

    실습

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,17 +1,14 @@
-name = 'Zed A. Shaw'
+my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie
-my_height_cm = 74 # inches
-cm_to_inch = 1.0/2.54
-my_height_inch = my_height_cm * cm_to_inch
-my_weight = 180.5 # lbs
+my_height = 74 # inches
+my_weight = 180 # lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
-#place holder
-print "Let's talk about %s." % name
-print "He's %g centimeters tall." % my_height_cm
-print "He's %d inches tall." % my_height_inch
-print "He's %g pounds heavy." % my_weight
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
